3e461d58e42c46ba3bab9e7ff187a0b79cdeaea7
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-03-15 21:22:47 -0400

    commit tree e5fc39f01bb1caa9783e22c98f4642e06ad8eec0

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const totalBatteries = batteryBatches.reduce(function(totalBatteries, currentValue) { 
+    return totalBatteries + currentValue
+})
+
 
 // Code your solution here
